22688ba23623c5a1483b1fed28ef941348c8f961
Jack Jack <jac@jack.fr>
2023-05-11 18:44:35 -0500

    yes

--- srcs/requirements/wordpress/tools/setup_and_start_wordpress_service.sh
+++ srcs/requirements/wordpress/tools/setup_and_start_wordpress_service.sh
@@ -1,23 +1,16 @@
-#!/bin/bash
-
-#Create(or recreate if it already exists) user 'www-data' that will be using the CGI pool.
+#Create user that will be used to run the CGIs.
 userdel www-data
 useradd -u 82 www-data
 chown -R www-data:www-data /var/www/html
 
-#Unpack wordpress.
-tar -xzf /var/www/html/latest.tar.gz -C /var/www/html 
+#Install  latest wordpress version.
+tar -xzf /var/www/html/latest.tar.gz -C /var/www/html
 
-if [ -d /var/www/html/wordpress ]; then
-	rm -rf /var/www/html/wordpress
+#Use WP-CLi to initialize website.
+chmod 777 /usr/local/bin/wp
+wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --allow-root --path=/var/www/html/wordpress
+wp core install --url=$DOMAIN_NAME --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root --path=/var/www/html/wordpress
+wp user create $USERNAME $USER_EMAIL --role=subscriber --user_pass=$USER_PASSWORD --allow-root --path=/var/www/html/wordpress
 
-fi
-#Use the wp CLI to setup the website.
-#if [ ! -d /var/www/html/wordpress ]; then
-	chmod 777 /usr/local/bin/wp
-	wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --allow-root --path=/var/www/html/wordpress
-	wp core install --url=$DOMAIN_NAME --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root --path=/var/www/html/wordpress
-	wp user create $USERNAME $USER_EMAIL --role=subscriber --user_pass=$USER_PASSWORD --allow-root --path=/var/www/html/wordpress
-#fi
-
+#Start the CGI pool.
 /usr/sbin/php-fpm7.3 --nodaemonize
